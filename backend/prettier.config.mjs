5e9a65001e3f24f6e37afd28a099abb7fc5172c5
Théo Vogel <contact@theovogel.fr>
2025-01-29 19:42:00 +0100

    Initialize backend with TypeScript, ESLint, and Prettier configuration

---
+++ backend/prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+function helloWorld() {
+  console.log(helloWorld);
+}
+
+helloWorld();
