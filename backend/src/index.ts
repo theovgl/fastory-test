9ea536d45547cf46a62f9119b1c28addf361dde2
Théo Vogel <contact@theovogel.fr>
2025-01-29 19:47:11 +0100

    Implement Hapi server initialization and error handling in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,18 @@
-function helloWorld() {
-  console.log(helloWorld);
-}
+import Hapi from "@hapi/hapi";
 
-helloWorld();
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
