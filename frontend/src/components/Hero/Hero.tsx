a9ab7d5ef0710f44370624693f6b62d715ba2056
Théo Vogel <contact@theovogel.fr>
2025-02-01 17:23:05 +0100

    Add Hero component

---
+++ frontend/src/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.heroContainer}>
+      <h1>Star Wars Rebels Alliance Search System</h1>
+    </div>
+  );
+}
