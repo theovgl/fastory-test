c1f75e9dcc99bb01ccfb1f05f7da5555274a0761
Théo Vogel <contact@theovogel.fr>
2025-02-03 23:26:02 +0100

    Add link to hero header

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router";
 import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
     <div className={styles.heroContainer}>
-      <h1>Star Wars Rebels Alliance Search System</h1>
+      <h1>
+        <Link to="/" className={styles.hero_title}>
+          Star Wars Rebels Alliance Search System
+        </Link>
+      </h1>
     </div>
   );
 }
