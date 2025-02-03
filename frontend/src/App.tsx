4d09e3968a3b4c069945f8d880877e8f84e45978
Théo Vogel <contact@theovogel.fr>
2025-02-03 23:26:57 +0100

    Add route for each page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
 import "./reset.css";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
-import SearchBar from "./components/SearchBar/SearchBar";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import FilmDetails from "./pages/FilmDetails";
+import PeopleDetails from "./pages/PeopleDetails";
+import PlanetDetails from "./pages/PlanetDetails";
+import SpeciesDetails from "./pages/SpeciesDetails";
+import StarshipsDetails from "./pages/StarshipDetails";
+import VehicleDetails from "./pages/VehicleDetails";
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <SearchBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/films/:id" element={<FilmDetails />} />
+        <Route path="/people/:id" element={<PeopleDetails />} />
+        <Route path="/planets/:id" element={<PlanetDetails />} />
+        <Route path="/species/:id" element={<SpeciesDetails />} />
+        <Route path="/starships/:id" element={<StarshipsDetails />} />
+        <Route path="/vehicles/:id" element={<VehicleDetails />} />
+      </Routes>
     </div>
   );
 }
